66be581225ab607a8d04d7f5a6bd5a0501ef9359
smirnovs <maneerlik@ya.ru>
2025-11-19 22:55:07 +0300

    spring-cloud: реализация паттернов для облачных сред исполнения

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,68 +1,24 @@
 package ru.practicum;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-import org.springframework.http.HttpStatusCode;
+import feign.FeignException;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-@Service
-@Slf4j
-@AllArgsConstructor
-public class StatsClient {
-    private final RestClient restClient;
-    private final String url;
+@FeignClient(name = "stats-server")
+public interface StatsClient {
+    @PostMapping("/hit")
+    String saveHit(@RequestBody HitDto hitDto) throws FeignException;
 
-    @Autowired
-    public StatsClient(@Value("${stats-server.url}") String serverUrl) {
-        log.info("url: " + serverUrl);
-        restClient = RestClient.builder()
-                .baseUrl(serverUrl)
-                .build();
-        url = serverUrl;
-    }
-
-    public ResponseEntity<Object> saveHit(HitDto hitDto) {
-        return restClient.post()
-                .uri(uriBuilder -> uriBuilder.path("/hit").build())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(hitDto)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, (req, res) -> {
-                    throw new RuntimeException("StatsService error: " + res.getStatusText());
-                })
-                .toEntity(Object.class);
-    }
-
-    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        String formattedStart = start.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        String formattedEnd = end.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-
-        ResponseEntity<List<ViewStatsDto>> response = restClient.get()
-                .uri(uriBuilder -> {
-                    uriBuilder.path("/stats")
-                            .queryParam("start", formattedStart)
-                            .queryParam("end", formattedEnd)
-                            .queryParam("unique", unique);
-
-                    if (uris != null && !uris.isEmpty()) {
-                        uriBuilder.queryParam("uris", String.join(",", uris));
-                    }
-
-                    return uriBuilder.build();
-                })
-                .retrieve()
-                .toEntity(new ParameterizedTypeReference<List<ViewStatsDto>>() {});
-
-        return response.getBody();
-    }
+    @GetMapping("/stats")
+    List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                @RequestParam(required = false) List<String> uris,
+                                @RequestParam(required = false) Boolean unique);
 }
--- stats/stats-dto/src/main/java/ru/practicum/HitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/HitDto.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,11 +13,9 @@ import java.time.LocalDateTime;
 public class HitDto {
     private Long id;
 
-    @NotNull
     @NotBlank
     private String app;
 
-    @NotNull
     @NotBlank
     private String uri;
     private String ip;
